7c768fab07c326d4ae964cc12410f585a6e67435
qiwzhu <zqwjulia55@hotmail.com>
2015-09-15 15:40:08 -0400

    Update level-001.py

--- level-001.py
+++ level-001.py
@@ -11,3 +11,10 @@ enc_text = 'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp.
 print unencrypt(enc_text)
 
 print unencrypt('map')
+
+
+# the output:
+# i hope you didnt translate it by hand. thats what computers are for.doing it in by hand is inefficient and that’s why this text is so long.using string.maketrans() is recommended. now apply on the url.
+# ocr
+
+# use 'ocr' to replace 'map' in the url and you get into the next level.
